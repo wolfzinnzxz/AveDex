60d811eff4df886c177e7e0ae1b379743a3b3124
Leonan Pinheiro Rosário Silva <leonan1807@gmail.com>
2026-08-07 12:30:55 -0300

    Adiciona consulta de detalhes por código

--- Avedex.py
+++ Avedex.py
@@ -9,7 +9,7 @@ def exibir_menu():
     exibir_linha()
     print("1 - Ver mensagem de boas-vindas")
     print("2 - Listar aves")
-    print("3 - Ver uma curiosidade sobre aves")
+    print("3 - Ver detalhes de uma ave")
     print("4 - Sobre a AveDex")
     print("0 - Sair")
 
@@ -30,9 +30,25 @@ def listar_aves(catalogo):
         print(f"{ave['codigo']} - {ave['nome_popular']}")
 
 
-def mostrar_curiosidade():
-    print("Curiosidade:")
-    print("Muitas aves ajudam no equilíbrio ambiental ao dispersar sementes.")
+def buscar_ave_por_codigo(catalogo, codigo_procurado):
+    for ave in catalogo:
+        if ave["codigo"] == codigo_procurado:
+            return ave
+
+    return None
+
+
+def exibir_detalhes(ave):
+    print()
+    exibir_linha()
+    print("DETALHES DA AVE")
+    exibir_linha()
+
+    print(f"Nome popular: {ave['nome_popular']}")
+    print(f"Nome científico: {ave['nome_cientifico']}")
+    print(f"Habitat: {ave['habitat']}")
+    print(f"Alimentação: {ave['alimentacao']}")
+    print(f"Curiosidade: {ave['curiosidade']}")
 
 
 def mostrar_sobre():
@@ -46,6 +62,7 @@ def pausar():
 
 
 # Catálogo de aves
+
 catalogo_aves = [
     {
         "codigo": "1",
@@ -75,6 +92,7 @@ catalogo_aves = [
 
 
 # Programa principal
+
 exibir_linha()
 print("AVEDEX")
 exibir_linha()
@@ -91,24 +109,44 @@ while opcao_menu != "0":
 
     print()
 
+
     if opcao_menu == "1":
         mostrar_boas_vindas(nome_usuario)
+
 
     elif opcao_menu == "2":
         listar_aves(catalogo_aves)
 
+
     elif opcao_menu == "3":
-        mostrar_curiosidade()
+        listar_aves(catalogo_aves)
+
+        codigo_escolhido = input("\nDigite o código da ave: ").strip()
+
+        ave_encontrada = buscar_ave_por_codigo(
+            catalogo_aves,
+            codigo_escolhido
+        )
+
+        if ave_encontrada is not None:
+            exibir_detalhes(ave_encontrada)
+
+        else:
+            print("Ave não encontrada. Confira o código informado.")
+
 
     elif opcao_menu == "4":
         mostrar_sobre()
+
 
     elif opcao_menu == "0":
         print("Encerrando a AveDex.")
         print(f"Até logo, {nome_usuario}!")
 
+
     else:
         print("Opção inválida. Digite apenas 0, 1, 2, 3 ou 4.")
+
 
     if opcao_menu != "0":
         pausar()
